473305f2196aa1f624a2f7cfd23e617f4907971b
IraDmitrieva <iritrieva@gmail.com>
2023-09-06 21:48:00 +0400

    port

---
+++ src/components/the-main-screen.vue
@@ -0,0 +1,15 @@
+<template>
+  <div>
+    <div class="introdusing">Front-End Developer</div>
+    <div class="bottom">
+      <div class="txt">Available for freelance projects</div>
+      <div class="txt">Live in Togliatti, Russia</div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {};
+</script>
+
+<style lang="scss" scoped></style>
---
+++ src/navigation/the-header.vue
@@ -0,0 +1,25 @@
+<template>
+  <header>
+    <div class="logo">Irina Dmitrieva</div>
+    <ul class="header-menu">
+      <li class="links">
+        <a href="#about">About</a>
+      </li>
+      <li class="links">
+        <a href="#skills">Skills</a>
+      </li>
+      <li class="links">
+        <a href="#projects">Projects</a>
+      </li>
+      <li class="links">
+        <a href="#contact">Contact</a>
+      </li>
+    </ul>
+  </header>
+</template>
+
+<script>
+export default {};
+</script>
+
+<style lang="scss" scoped></style>
